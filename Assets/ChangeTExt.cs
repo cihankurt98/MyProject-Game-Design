e564718f11109f7dda12f4cc9e86413bfc809061
Cihan <cihankurt1998@gmail.com>
2018-03-08 15:27:21 +0100

    buttonchangetext
    De button veranderd de text op de label

---
+++ Assets/ChangeTExt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ChangeTExt : MonoBehaviour
+{
+
+    public Text mytext = null;
+    public int counter = 0;
+    public void changeText()
+    {
+        counter++;
+        mytext.text = "Gold: " + Convert.ToString(counter);
+    }
+}
